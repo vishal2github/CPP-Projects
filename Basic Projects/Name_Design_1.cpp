523edcbb11baa6bca1e15e37d46b8debe8ecdb6e
Vishal Bhardwaj <vishalbhardwaj9400@gmail.com>
2024-01-10 10:02:53 +0530

    Name-design program added with an endl elimination

---
+++ Basic Projects/Name_Design_1.cpp
@@ -0,0 +1,47 @@
+// THE PROGRAM TO PRINT NAME-DESIGN.
+
+#include <iostream>
+
+using std::cout;
+
+int main()
+{
+       cout << "\n" << "Hello world!\n\n";
+
+       cout << "00      00   0000000000   0000000000   00      00   0000000000\n";
+       cout << "00      00       00       00           00      00       00    \n";
+       cout << " 00    00        00       00           00  00  00       00    \n";
+       cout << "  00  00         00          0000      00  00  00       00    \n";
+       cout << "   0000          00               00   00      00       00    \n";
+       cout << "    00       0000000000   0000000000   00      00   0000000000\n\n";
+
+       cout << "0000000000\n";
+       cout << "    00    \n";
+       cout << "    00    \n";
+       cout << "    00    \n";
+       cout << "    00    \n";
+       cout << "0000000000\n\n";
+ 
+       cout << "0000000000\n";
+       cout << "00        \n";
+       cout << "00        \n";
+       cout << "   0000   \n";
+       cout << "        00\n";
+       cout << "0000000000\n\n";
+ 
+       cout << "00      00\n";
+       cout << "00      00\n";
+       cout << "00  00  00\n";
+       cout << "00  00  00\n";
+       cout << "00      00\n";
+       cout << "00      00\n\n";    
+ 
+       cout << "0000000000\n";
+       cout << "    00    \n";
+       cout << "    00    \n";
+       cout << "    00    \n";
+       cout << "    00    \n";
+       cout << "0000000000\n\n";
+
+       return 0;
+}
