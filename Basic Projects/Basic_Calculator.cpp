970d268271873848605a875a94f6418abc141853
Vishal Bhardwaj <vishalbhardwaj9400@gmail.com>
2024-01-10 12:22:54 +0530

    Eliminated function prototypes; Changed about

--- Basic Projects/Basic_Calculator.cpp
+++ Basic Projects/Basic_Calculator.cpp
@@ -1,4 +1,8 @@
-// A PROGRAM TO CALCULATE AND PRINT THE RESULT OF TWO VALUES (DEC OR INT) USING 4 BASIC MATH OPERATIONS.
+/*
+    BASIC CALCULATOR
+        
+        It is based on the four simple mathematical calulations on the two integer or decimal values and the print the required result. Only one mathematical operations can be performed among all four operations at a time.
+*/
 
 #include <iostream>
 #include <windows.h>
@@ -6,59 +10,8 @@
 using std::cin;
 using std::cout;
 
-void add();
-void sub();
-void mul();
-void div();
-
 char ch;
 float a, b, result;
-
-int main()
-{
-    system("cls");
-
-    cout << "\nEnter any two numbers whether in form of an integer, decimal or a combination of two.";
-
-    Sleep(2000);
-
-    cout << "\nThe operation is performed on the basis of operator selected.";
-
-    Sleep(2000);
-
-    cout << "\n\nENTER THE OPERATOR (/,*,+,-) : ";
-    cin >> ch;
-    cout << "\n";
-
-    if (ch == '+')
-    {
-        add();
-    }
-    else if (ch == '-')
-    {
-        sub();
-    }
-    else if (ch == '*')
-    {
-        mul();
-    }
-    else if (ch == '/')
-    {
-        div();
-    }
-    else
-    {
-        cout << "WRONG OPERATOR SELECTED! PLEASE SELECT FROM THE GIVEN OPERATORS ONLY!\n\n";
-    }
-
-    Sleep(2000);
-
-    cout << "\nPROCESS TERMINATED!\n\n";
-
-    system("pause");
-
-    return 0;
-}
 
 void add()
 {
@@ -122,4 +75,50 @@ void div()
     r = (a / b);
 
     cout << "\nThe division of " << a << " and " << b << " is " << r << ".\n";
+}
+
+int main()
+{
+    system("cls");
+
+    cout << "\nEnter any two numbers whether in form of an integer, decimal or a combination of two.";
+
+    Sleep(2000);
+
+    cout << "\nThe operation is performed on the basis of operator selected.";
+
+    Sleep(2000);
+
+    cout << "\n\nENTER THE OPERATOR (/,*,+,-) : ";
+    cin >> ch;
+    cout << "\n";
+
+    if (ch == '+')
+    {
+        add();
+    }
+    else if (ch == '-')
+    {
+        sub();
+    }
+    else if (ch == '*')
+    {
+        mul();
+    }
+    else if (ch == '/')
+    {
+        div();
+    }
+    else
+    {
+        cout << "WRONG OPERATOR SELECTED! PLEASE SELECT FROM THE GIVEN OPERATORS ONLY!\n\n";
+    }
+
+    Sleep(2000);
+
+    cout << "\nPROCESS TERMINATED!\n\n";
+
+    system("pause");
+
+    return 0;
 }
