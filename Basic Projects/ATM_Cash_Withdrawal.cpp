97e4abf1f9e1c79419e0b7771253b22768dccd11
Vishal Bhardwaj <vishalbhardwaj9400@gmail.com>
2024-01-18 10:28:09 +0530

    Update: Zero-amount (or negative amount) error display added

--- Basic Projects/ATM_Cash_Withdrawal.cpp
+++ Basic Projects/ATM_Cash_Withdrawal.cpp
@@ -14,81 +14,106 @@
 using std::cin;
 using std::cout;
 
+class Debit
+{
+private:
+     int card, pin, amt, i;
+
+public:
+     void transaction_process(void);
+};
+
+void Debit ::transaction_process()
+{
+     cout << "\n[*PLEASE ENTER YOUR DEBIT-CARD IN THE SLOT*]"
+          << "\n";
+
+     cout << "\nENTER '1' FOR THE CARD DETECTION."
+          << "\n";
+     cin >> card;
+
+     if (card == 1)
+     {
+          Sleep(2000);
+
+          cout << "CARD DETECTION IN PROCESS..."
+               << "\n";
+
+          Sleep(2000);
+
+          cout << "CARD DETECTED!"
+               << "\n";
+
+          Sleep(2000);
+
+          cout << "\nENTER THE PIN BELOW TO PROCEED:"
+               << "\n";
+          cin >> pin;
+
+          if (pin == 5555) // Supposed debit card pin is 5555
+          {
+               Sleep(2000);
+
+               cout << "ENTER THE WITHDRAW AMOUNT:"
+                    << "\n";
+               cout << "$ ";
+               cin >> amt;
+               Sleep(2000);
+
+               if (amt >= 1)
+               {
+                    cout << "\nSYSTEM IS PROCESSING THE AMOUNT. PLEASE WAIT..."
+                         << "\n";
+
+                    Sleep(5000);
+
+                    cout << "\n$" << amt << " HAS BEEN DISPENSED IN TRAY."
+                         << "\n";
+
+                    Sleep(2000);
+
+                    cout << "TRANSACTION COMPLETED. PLEASE COLLECT YOUR CARD.\n";
+                    Sleep(2000);
+                    cout << "THANK YOU FOR USING OUR ATM SERVICE.\n";
+                    Sleep(500);
+               }
+               else
+               {
+                    cout << "AMOUNT CAN'T BE LESS THAN OR EQUAL TO ZERO\n";
+                    Sleep(1500);
+                    cout << "PLEASE RETRY!\n";
+               }
+          }
+          else
+          {
+               Sleep(2000);
+
+               cout << "\nWRONG PIN ENTERED. PLEASE TRY AGAIN."
+                    << "\n";
+          }
+
+          cout << "\n";
+     }
+     else
+     {
+          Sleep(2000);
+
+          cout << "CARD DETECTION IN PROCESS..."
+               << "\n";
+
+          Sleep(2000);
+
+          cout << "\nNO CARD DETECTED. PLEASE TAKE OUT CARD AND RETRY."
+               << "\n";
+     }
+}
+
 int main()
 {
-    int card, pin, amt, i;
+     system("cls");
 
-    cout << "\n[*PLEASE ENTER YOUR DEBIT-CARD IN THE SLOT*]"
-         << "\n";
+     Debit card1;
+     card1.transaction_process();
 
-    cout << "\nENTER '1' FOR THE CARD DETECTION."
-         << "\n";
-    cin >> card;
-
-    if (card == 1)
-    {
-        Sleep(2000);
-
-        cout << "CARD DETECTION IN PROCESS..."
-             << "\n";
-
-        Sleep(2000);
-
-        cout << "CARD DETECTED!"
-             << "\n";
-
-        Sleep(2000);
-
-        cout << "\nENTER THE PIN BELOW TO PROCEED:"
-             << "\n";
-        cin >> pin;
-
-        if (pin == 5555) // Supposed debit card pin is 5555
-        {
-            Sleep(2000);
-
-            cout << "ENTER THE WITHDRAW AMOUNT:"
-                 << "\n";
-            cout << "$ ";
-            cin >> amt;
-
-            Sleep(2000);
-
-            cout << "\nSYSTEM IS PROCESSING THE AMOUNT. PLEASE WAIT..."
-                 << "\n";
-
-            Sleep(5000);
-
-            cout << "\n$" << amt << " HAS BEEN DISPENSED IN TRAY."
-                 << "\n";
-
-            Sleep(2000);
-
-            cout << "TRANSACTION COMPLETED. PLEASE COLLECT YOUR CARD."
-                 << "\n";
-        }
-        else
-        {
-            Sleep(2000);
-
-            cout << "\nWRONG PIN ENTERED. PLEASE TRY AGAIN."
-                 << "\n";
-        }
-
-        cout << "\n";
-    }
-    else
-    {
-        Sleep(2000);
-
-        cout << "CARD DETECTION IN PROCESS..."
-             << "\n";
-
-        Sleep(2000);
-
-        cout << "\nNO CARD DETECTED. PLEASE TAKE OUT CARD AND RETRY."
-             << "\n";
-    }
-
-    return 0;
+     return 0;
 }
