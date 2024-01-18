38866caeee2ad439dacde64739dcfa22fd427c6f
Vishal Bhardwaj <vishalbhardwaj9400@gmail.com>
2024-01-18 11:03:11 +0530

    Private data safety enhanced

--- Basic Projects/Basic_Calculator.cpp
+++ Basic Projects/Basic_Calculator.cpp
@@ -14,79 +14,89 @@ class Calculator
 {
 private:
     float a, b, result;
+    void add(void);
+    void sub(void);
+    void mul(void);
+    void div(void);
 
 public:
-    void add()
-    {
-        cout << ":: ADDITION OPERATION SELECTED ::\n";
-
-        float a, b, r;
-        cout << "\nENTER TWO NUMBERS";
-        cout << "\nThe first number is: ";
-        cin >> a;
-        cout << "The second number is: ";
-        cin >> b;
-
-        r = (a + b);
-
-        cout << "\nThe addition of " << a << " and " << b << " is " << r << ".\n";
-    }
-
-    void sub()
-    {
-        cout << ":: SUBTRACTION OPERATION SELECTED ::\n";
-
-        float a, b, r;
-        cout << "\nENTER TWO NUMBERS";
-        cout << "\nThe first number is: ";
-        cin >> a;
-        cout << "The second number is: ";
-        cin >> b;
-
-        r = (a - b);
-
-        cout << "\nThe subtraction of " << a << " and " << b << " is " << r << ".\n";
-    }
-
-    void mul()
-    {
-        cout << ":: MULTIPLICATION OPERATION SELECTED ::\n";
-
-        float a, b, r;
-        cout << "\nENTER TWO NUMBERS";
-        cout << "\nThe first number is: ";
-        cin >> a;
-        cout << "The second number is: ";
-        cin >> b;
-
-        r = (a * b);
-
-        cout << "\nThe multiplication of " << a << " and " << b << " is " << r << ".\n";
-    }
-
-    void div()
-    {
-        cout << ":: DIVISION OPERATION SELECTED ::\n";
-
-        float a, b, r;
-        cout << "\nENTER TWO NUMBERS";
-        cout << "\nThe first number is: ";
-        cin >> a;
-        cout << "The second number is: ";
-        cin >> b;
-
-        r = (a / b);
-
-        cout << "\nThe division of " << a << " and " << b << " is " << r << ".\n";
-    }
+    void calc_process(void);
 };
 
-int main()
+void Calculator ::add()
 {
-    system("cls");
+    cout << ":: ADDITION OPERATION SELECTED ::\n";
 
-    Calculator c1;
+    float a, b, r;
+    cout << "\nENTER TWO NUMBERS";
+    cout << "\nThe first number is: ";
+    cin >> a;
+    cout << "The second number is: ";
+    cin >> b;
 
+    r = (a + b);
+
+    cout << "\nThe addition of " << a << " and " << b << " is " << r << ".\n";
+}
+
+void Calculator ::sub()
+{
+    cout << ":: SUBTRACTION OPERATION SELECTED ::\n";
+
+    float a, b, r;
+    cout << "\nENTER TWO NUMBERS";
+    cout << "\nThe first number is: ";
+    cin >> a;
+    cout << "The second number is: ";
+    cin >> b;
+
+    r = (a - b);
+
+    cout << "\nThe subtraction of " << a << " and " << b << " is " << r << ".\n";
+}
+
+void Calculator ::mul()
+{
+    cout << ":: MULTIPLICATION OPERATION SELECTED ::\n";
+
+    float a, b, r;
+    cout << "\nENTER TWO NUMBERS";
+    cout << "\nThe first number is: ";
+    cin >> a;
+    cout << "The second number is: ";
+    cin >> b;
+
+    r = (a * b);
+
+    cout << "\nThe multiplication of " << a << " and " << b << " is " << r << ".\n";
+}
+
+void Calculator ::div()
+{
+    cout << ":: DIVISION OPERATION SELECTED ::\n";
+
+    float a, b, r;
+    cout << "\nENTER TWO NUMBERS";
+    cout << "\nThe first number is: ";
+    cin >> a;
+    cout << "The second number is: \n";
+    cin >> b;
+
+    if (a == 0)
+    {
+        cout << "Zero isn't divisible!\n\n";
+        exit(0);
+    }
+    else
+    {
+        r = (a / b);
+    }
+
+    cout << "The division of " << a << " and " << b << " is " << r << ".\n";
+}
+
+void Calculator ::calc_process()
+{
     char ch;
     cout << "\nEnter any two numbers whether in form of an integer, decimal or a combination of two.";
     Sleep(2000);
@@ -99,19 +109,19 @@ int main()
 
     if (ch == '+')
     {
-        c1.add();
+        add();
     }
     else if (ch == '-')
     {
-        c1.sub();
+        sub();
     }
     else if (ch == '*')
     {
-        c1.mul();
+        mul();
     }
     else if (ch == '/')
     {
-        c1.div();
+        div();
     }
     else
     {
@@ -120,6 +130,14 @@ int main()
 
     Sleep(2000);
     cout << "\nPROCESS TERMINATED!\n\n";
+}
+
+int main()
+{
+    system("cls");
+
+    Calculator c1;
+    c1.calc_process();
 
     system("pause");
 
