c83ea0cde83656267ec0467d915634790cd1cc88
Vishal Bhardwaj <vishalbhardwaj9400@gmail.com>
2024-01-19 08:45:56 +0530

    Variables made private

--- Basic Projects/Multiplication_Table.cpp
+++ Basic Projects/Multiplication_Table.cpp
@@ -1,7 +1,7 @@
 /*
     MULTIPLICATION TABLE PROJECT
         > A simple project based on mathematical table of multiplication with an unlimited multiplication range.
-    
+
     IMPORTANT NOTE
         > Use 'Ctrl + C', to terminate the execution process, in order to avoid any harm to your system in case of an accidental execution with the objectionable multiplication-range that your system is incapable of handeling!
 */
@@ -11,32 +11,40 @@
 using std::cin;
 using std::cout;
 
-int i, mul;
-float num;
+class Table
+{
+private:
+    int i, mul;
+    float num;
 
-void maths_table()
+public:
+    void maths_table(void);
+};
+
+void Table ::maths_table()
 {
     cout << "Enter the multiplier-number of a required table: ";
     cin >> num;
     cout << "Enter the multiplication range of a required table: ";
     cin >> mul;
 
-    cout << "\nTHE TABLE OF " << num << " IS AS FOLLOWS:" << "\n\n";
+    cout << "\nTHE TABLE OF " << num << " IS AS FOLLOWS:"
+         << "\n\n";
 
     for (i = 1; i <= mul; i++)
     {
         cout << num << " x " << i << " = " << num * i << "\n";
     }
+
+    cout << "\nPROCESS TERMINATED!\n\n";
 }
 
 int main()
 {
     system("cls");
 
-    maths_table();
-
-    cout << "\nPROCESS TERMINATED!\n"
-         << "\n";
+    Table t1;
+    t1.maths_table();
 
     system("pause");
 
